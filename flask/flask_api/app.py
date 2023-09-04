05dfff386b60b41006e1f5a5a7692aa1aaa8d935
Janis Stals <stalsjanis@gmail.com>
2023-09-04 10:52:26 +0300

    flask update

--- flask/flask_api/app.py
+++ flask/flask_api/app.py
@@ -4,7 +4,11 @@ from transaction import transactionObject
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def hello_world():
     msg = transactionObject()
     return msg
+
+# app.run()
+
+# gunicorn --bind 0.0.0.0:5000 -w 8 wsgi:app
